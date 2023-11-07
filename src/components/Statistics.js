43aaa33ecf63a3d1a24a5a99e9813b8e152932cd
Roy Gonen <roy@armis.com>
2023-11-07 16:47:06 +0200

    changes

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,31 +1,12 @@
 import React from 'react';
 
 function Statistics({ correctAnswers, totalQuestions }) {
-  // Calculate the percentage of correct answers
-  const percent = (correctAnswers / totalQuestions) * 100;
-
-  // Determine the progress bar color based on the percentage
-  let progressBarColor = '';
-  if (percent < 20) {
-    progressBarColor = 'red';
-  } else if (percent >= 20 && percent < 50) {
-    progressBarColor = 'orange';
-  } else if (percent >= 50 && percent < 80) {
-    progressBarColor = 'yellow';
-  } else {
-    progressBarColor = 'green';
-  }
+  const percent = Math.round((correctAnswers / totalQuestions) * 100);
 
   return (
     <div className="statistics-container">
-      <h2>Congratulations!</h2>
-      <p>You got {percent}% correct</p>
-      <div className="progress-bar">
-        <div
-          className="progress"
-          style={{ width: `${percent}%`, backgroundColor: progressBarColor }}
-        />
-      </div>
+      <h2>!כל הכבוד</h2>
+      <p>צדקת ב {percent}% מהתשובות</p>
     </div>
   );
 }
